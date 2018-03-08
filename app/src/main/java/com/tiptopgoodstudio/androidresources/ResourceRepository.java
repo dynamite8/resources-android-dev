a1b061a2f36fa5cd4612b9a3129d8fa52a6c75b2
Somayah88 <somayah.alharbi@gmail.com>
2018-03-08 12:19:18 -0500

    Fixes #42: Update Resource Dao

--- app/src/main/java/com/tiptopgoodstudio/androidresources/ResourceRepository.java
+++ app/src/main/java/com/tiptopgoodstudio/androidresources/ResourceRepository.java
@@ -3,6 +3,9 @@ package com.tiptopgoodstudio.androidresources;
 
 import android.app.Application;
 import android.arch.lifecycle.LiveData;
+import android.arch.persistence.db.SupportSQLiteOpenHelper;
+import android.arch.persistence.room.DatabaseConfiguration;
+import android.arch.persistence.room.InvalidationTracker;
 
 import com.tiptopgoodstudio.androidresources.db.AppDatabase;
 import com.tiptopgoodstudio.androidresources.db.dao.ResourceDao;
@@ -16,10 +19,16 @@ public class ResourceRepository {
     private LiveData<List<Resource>> mAllResources;
 
     ResourceRepository(Application application) {
-
+        AppDatabase db=AppDatabase.getDatabase(application);
+        mResourceDao=db.resourceDao();
+        mAllResources=mResourceDao.getResources();
     }
 
+LiveData<List<Resource>> getResouces()
+{
+    return mAllResources;
+}
+public void insertResource(Resource resource){
 
-
-
+}
 }
