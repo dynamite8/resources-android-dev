9bbcf6e275a133ed1dd8d0e56691e6837675bd9d
Somayah88 <somayah.alharbi@gmail.com>
2018-03-08 12:15:17 -0500

    Fixes #42: Update Resource Dao

--- app/src/main/java/com/tiptopgoodstudio/androidresources/db/AppDatabase.java
+++ app/src/main/java/com/tiptopgoodstudio/androidresources/db/AppDatabase.java
@@ -17,7 +17,7 @@ public abstract class AppDatabase extends RoomDatabase {
 
     public abstract ResourceDao resourceDao();
 
-    static AppDatabase getDatabase(final Context context) {
+    public static AppDatabase getDatabase(final Context context) {
         if (INSTANCE == null) {
             synchronized (AppDatabase.class) {
                 if (INSTANCE == null) {
