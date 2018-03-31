30d1024192eea9926ad302c835368195e7302839
JaeW <janice.wildrick@gmail.com>
2018-03-31 02:03:30 -0500

    Correct conflict errors made during pull request merge

--- app/src/main/java/com/tiptopgoodstudio/androidresources/db/dao/ResourceDao.java
+++ app/src/main/java/com/tiptopgoodstudio/androidresources/db/dao/ResourceDao.java
@@ -1,7 +1,5 @@
 package com.tiptopgoodstudio.androidresources.db.dao;
 
-
-
 import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
